d91a456604592c97184ff7c53f01589309b6357f
ThinThinzarHtet <thinthinzarhtet19@gmail.com>
2026-06-01 13:05:17 +0630

    add suspense boundary

--- src/app/auth/change-password/page.tsx
+++ src/app/auth/change-password/page.tsx
@@ -1,7 +1,12 @@
+import { Suspense } from "react";
 import ChangePasswordForm from "@/features/auth/components/ChangePasswordForm";
 
 function ChangePassword() {
-  return <ChangePasswordForm />;
+  return (
+    <Suspense fallback={<p className="text-black dark:text-white">Loading...</p>}>
+      <ChangePasswordForm />
+    </Suspense>
+  );
 }
 
 export default ChangePassword;
